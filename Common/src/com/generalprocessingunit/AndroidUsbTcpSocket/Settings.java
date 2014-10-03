e0b264323e25db3181a507ae66879deea366fc83
Paul M. Christian <paulmchristian@gmail.com>
2014-10-03 17:42:57 -0500

    only need path to ADB in settings

--- Common/src/com/generalprocessingunit/AndroidUsbTcpSocket/Settings.java
+++ Common/src/com/generalprocessingunit/AndroidUsbTcpSocket/Settings.java
@@ -2,5 +2,5 @@ package com.generalprocessingunit.AndroidUsbTcpSocket;
 
 public class Settings {
     public static final int PORT = 38300;
-    public static final String ADB_CMD = "C:\\Dev Tools\\adt-bundle-windows-x86_64\\sdk\\platform-tools\\adb.exe forward tcp:%s tcp:%s";
+    public static final String ADB_PATH = "D:\\DevTools\\adt-bundle-windows-x86_64-20140321\\sdk\\platform-tools\\adb.exe";
 }
